c959205a2ece89d8ddf4d0e829139671033634c5
Ole <ole@kallerud.no>
2024-11-03 21:13:11 +0100

    Update login page ui

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,74 +1,107 @@
 <script lang="ts">
   import { enhance } from '$app/forms'
+  import { Button } from '$lib/components/ui/button/index.js'
+  import * as Card from '$lib/components/ui/card/index.js'
+  import { Input } from '$lib/components/ui/input/index.js'
+  import { Label } from '$lib/components/ui/label/index.js'
   import type { ActionData, PageData } from './$types'
   import type { FormMode } from './+page.server'
 
   let { form }: { data: PageData; form: ActionData } = $props()
 
   let mode = $state<FormMode>('lookup')
-  const submitButtonText = $derived(
-    mode == 'lookup' ? 'Fortsett' : mode === 'register' ? 'Registrer' : 'Logg inn'
-  )
+  let details = $derived(getDetails(mode))
 
   $effect(() => {
     if (!form) return
     console.debug('form', form)
     if (form.formMode) mode = form.formMode
+    console.debug('mode', mode)
   })
+
+  function getDetails(mode: FormMode) {
+    if (mode === 'lookup')
+      return {
+        title: 'Velkommen',
+        description: 'Skriv inn mailen din for å logge inn eller opprette en bruker.',
+        button: 'Fortsett',
+      }
+
+    if (mode === 'register')
+      return {
+        title: 'Registrer deg',
+        description: 'Fant ingen bruker med den mailen, vil du registrere deg?',
+        button: 'Registrer',
+      }
+
+    return {
+      title: 'Logg inn',
+      description: 'Skriv inn passordet ditt for å logge inn.',
+      button: 'Logg inn',
+    }
+  }
 </script>
 
-<svelte:head>
-  <title>Logg inn</title>
-</svelte:head>
-
-<div class="auth-page">
-  <div class="page container">
-    <div class="row">
-      <div class="col-md-6 offset-md-3 col-xs-12">
-        <h1 class="text-xs-center">Logg inn</h1>
-        <form
-          method="POST"
-          use:enhance={() =>
-            async ({ update }) =>
-              update({ reset: false })}
-          action={`?/${mode}`}
-        >
-          <fieldset class="form-group">
-            <input
-              class="form-control form-control-lg"
-              name="email"
-              type="email"
-              required
-              placeholder="E-post"
-              autocomplete="email"
-            />
-          </fieldset>
-          <fieldset class="form-group">
-            <input
-              class="form-control form-control-lg"
+<div class="flex h-screen items-center justify-center">
+  <Card.Root class="w-full max-w-sm">
+    <Card.Header>
+      <Card.Title class="text-2xl">{details.title}</Card.Title>
+      <Card.Description>{details.description}</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <form
+        id="login-form"
+        class="grid gap-4"
+        method="POST"
+        use:enhance={() =>
+          async ({ update }) =>
+            update({ reset: false })}
+        action={`?/${mode}`}
+      >
+        <fieldset class="form-group grid gap-2">
+          <Label for="email">E-post</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autocomplete="email"
+            placeholder="jeghar@litentiss.no"
+            disabled={mode !== 'lookup'}
+          />
+        </fieldset>
+        {#if mode === 'register'}
+          <fieldset class="form-group grid gap-2">
+            <Label for="name">Navn</Label>
+            <Input
+              id="name"
               name="name"
               type="text"
-              hidden={mode !== 'register'}
-              required={mode === 'register'}
-              placeholder="Navn"
+              required
+              placeholder="Judas"
+              minlength={2}
               autocomplete="name"
             />
           </fieldset>
-          <fieldset class="form-group">
-            <input
-              class="form-control form-control-lg"
+        {/if}
+        {#if mode !== 'lookup'}
+          <fieldset class="form-group grid gap-2">
+            <Label for="password">Passord</Label>
+            <Input
+              id="password"
               name="password"
               type="password"
-              hidden={mode === 'lookup'}
-              required={mode !== 'lookup'}
-              placeholder="Passord"
-              minlength={8}
-              autocomplete={'current-password'}
+              required
+              placeholder="passord123"
+              minlength={6}
+              autocomplete={mode === 'login' ? 'new-password' : 'current-password'}
             />
           </fieldset>
-          <button class="btn btn-lg btn-primary pull-xs-right">{submitButtonText}</button>
-        </form>
-      </div>
-    </div>
-  </div>
+        {/if}
+      </form>
+    </Card.Content>
+    <Card.Footer>
+      <Button class="w-full" form="login-form" type="submit">{details.button}</Button>
+    </Card.Footer>
+  </Card.Root>
 </div>
