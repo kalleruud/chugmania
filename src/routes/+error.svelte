89b3bb684de2871a45366aa2f18ad6dda17d35d2
Ole Kallerud <ole@kallerud.no>
2024-11-06 00:39:22 +0100

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+  import * as Card from '$lib/components/ui/card/index.js'
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+  <Card.Root class="w-full max-w-sm">
+    <Card.Content>
+      <h1>{$page.status}</h1>
+      <Card.Description>{$page.error?.message}</Card.Description>
+    </Card.Content>
+  </Card.Root>
+</div>
