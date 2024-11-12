6e5da61f1300a3bdc23da7444c54e1845375bf60
Ole Kallerud <ole@kallerud.no>
2024-11-13 00:37:32 +0100

    Update track-lookup.svelte

--- src/lib/components/track-lookup/track-lookup.svelte
+++ src/lib/components/track-lookup/track-lookup.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { Button } from '$lib/components/ui/button/index.js'
-  import * as Command from '$lib/components/ui/command/index.js'
   import * as Popover from '$lib/components/ui/popover/index.js'
   import Search from 'lucide-svelte/icons/search'
   import { tick } from 'svelte'
