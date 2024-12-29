99f359a5dc00f60b72f9004720034bce0412204f
Ole Kallerud <ole@kallerud.no>
2024-12-29 14:46:04 +0100

    Minor fixes on swssion page

--- src/routes/sessions/+page.svelte
+++ src/routes/sessions/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { enhance } from '$app/forms'
-  import { Button } from '$lib/components/ui/button/index.js'
   import * as Card from '$lib/components/ui/card/index.js'
   import ChevronRight from 'lucide-svelte/icons/chevron-right'
   import Flag from 'lucide-svelte/icons/flag'
@@ -17,21 +15,18 @@
   <main class="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
     <div class="flex justify-between">
       <h1>Sessions</h1>
-      <form use:enhance method="POST" action={`?/create`}>
-        <Button type="submit">New</Button>
-      </form>
     </div>
     {#each data.sessions as session}
       <a href={'sessions/' + session.id}>
         <Card.Root class="flex items-center justify-between p-6 transition-colors hover:bg-accent">
           <div class="flex gap-2">
-            <Flag class="h-6 w-6" />
+            <Flag class="size-6" />
             <div>
               <Card.Title>{session.description ?? session.typeString}</Card.Title>
               <Card.Description>{relativeDay(session.date)}</Card.Description>
             </div>
           </div>
-          <ChevronRight class="h-6 w-6" />
+          <ChevronRight class="size-6" />
         </Card.Root>
       </a>
     {/each}
