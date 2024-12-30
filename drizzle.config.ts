da29c3428e3cf44a8e3831b3c39bfbbc19ad1b29
Ole <ole@kallerud.no>
2024-12-30 17:54:58 +0100

    Add schema to drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,13 +3,12 @@ import { defineConfig } from 'drizzle-kit'
 if (!process.env.DB_FILE_NAME) throw new Error('Missing environment variable: DB_FILE_NAME')
 
 export default defineConfig({
-  schema: './src/lib/server/db/schema.ts',
-
   dbCredentials: {
     url: 'data/' + process.env.DB_FILE_NAME,
   },
 
   verbose: true,
   strict: true,
+  schema: './src/server/db/schema.ts',
   dialect: 'sqlite',
 })
