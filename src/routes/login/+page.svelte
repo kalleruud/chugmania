5c9b0f121213036f98e002243dbebe06db2bf029
Ole Kallerud <ole@kallerud.no>
2024-12-30 02:36:00 +0100

    Fix login page jank

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,13 +10,13 @@
     mode === 'register'
       ? {
           title: 'Registrer deg',
-          description: 'Dytt inn infoen din, så er du klar til å chugge på null tid.',
-          button: 'Registrer',
+          description: 'Dytt inn infoen din, så er du klar til å chugge på null tid',
+          button: 'Kroppen min er klar',
         }
       : {
           title: 'Nættopp',
-          description: 'Skriv inn mailen din for å logge inn.',
-          button: 'Logg inn',
+          description: 'Skriv inn mailen din for å logge inn',
+          button: 'Trykk her hvis du har liten tiss',
         }
   )
 </script>
@@ -25,79 +25,79 @@
   <title>Chugmania - {details.title}</title>
 </svelte:head>
 
-<div class="fixed bottom-32 w-full touch-none select-none">
-  <div class="rounded-lg bg-secondary p-1">
-    <button
-      type="button"
-      class="rounded-sm py-1"
-      class:bg-background={mode === 'login'}
-      class:text-muted-foreground={mode !== 'login'}
-      onclick={() => (mode = 'login')}
-    >
-      Logg inn
-    </button>
-    <button
-      type="button"
-      class="rounded-sm py-1"
-      class:bg-background={mode === 'register'}
-      class:text-muted-foreground={mode !== 'register'}
-      onclick={() => (mode = 'register')}
-    >
-      Registrer
-    </button>
-  </div>
-</div>
-<div class="flex h-dvh touch-none flex-col justify-center p-4 pb-36">
-  <h1>{details.title}</h1>
-  <p class="text-muted-foreground">{details.description}</p>
+<div class="flex h-dvh w-full touch-none flex-col items-center justify-center">
+  <div class="w-full max-w-2xl p-4 pb-36">
+    <div class="mx-8 mb-8 flex rounded-lg bg-secondary p-1">
+      <button
+        type="button"
+        class="flex-1 rounded-sm py-1"
+        class:bg-background={mode === 'login'}
+        class:text-muted-foreground={mode !== 'login'}
+        onclick={() => (mode = 'login')}
+      >
+        <p>Logg inn</p>
+      </button>
+      <button
+        type="button"
+        class="flex-1 rounded-sm py-1"
+        class:bg-background={mode === 'register'}
+        class:text-muted-foreground={mode !== 'register'}
+        onclick={() => (mode = 'register')}
+      >
+        Registrer
+      </button>
+    </div>
+    <h1>{details.title}</h1>
+    <p class="text-muted-foreground">{details.description}</p>
 
-  <form
-    data-testid="login-form"
-    id="login-form"
-    class="mt-4 grid w-full gap-4"
-    method="POST"
-    use:enhance={() =>
-      async ({ update }) =>
-        await update({ reset: false })}
-    action={`?/${mode}`}
-  >
-    <fieldset class="form-group grid gap-2">
-      <Label for="email">E-post</Label>
-      <Input
-        id="email"
-        name="email"
-        type="email"
-        required
-        autocomplete="email"
-        placeholder="jeghar@litentiss.no"
-      />
-    </fieldset>
-    {#if mode === 'register'}
+    <form
+      data-testid="login-form"
+      id="login-form"
+      class="mt-4 grid w-full gap-4"
+      method="POST"
+      use:enhance={() =>
+        async ({ update }) =>
+          await update({ reset: false })}
+      action={`?/${mode}`}
+    >
       <fieldset class="form-group grid gap-2">
-        <Label for="name">Navn</Label>
+        <Label for="email">E-post</Label>
         <Input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Judas"
-          minlength={3}
-          autocomplete="name"
+          id="email"
+          name="email"
+          type="email"
+          required
+          autocomplete="email"
+          placeholder="jeghar@litentiss.no"
         />
       </fieldset>
-    {/if}
-    <fieldset class="form-group grid gap-2">
-      <Label for="password">Passord</Label>
-      <Input
-        id="password"
-        name="password"
-        type="password"
-        required={mode !== 'lookup'}
-        placeholder="passord123"
-        minlength={6}
-        autocomplete={mode === 'login' ? 'new-password' : 'current-password'}
-      />
-    </fieldset>
+      {#if mode === 'register'}
+        <fieldset class="form-group grid gap-2">
+          <Label for="name">Navn</Label>
+          <Input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Judas"
+            minlength={3}
+            autocomplete="name"
+          />
+        </fieldset>
+      {/if}
+      <fieldset class="form-group grid gap-2">
+        <Label for="password">Passord</Label>
+        <Input
+          id="password"
+          name="password"
+          type="password"
+          required={mode !== 'lookup'}
+          placeholder="passord123"
+          minlength={6}
+          autocomplete={mode === 'login' ? 'new-password' : 'current-password'}
+        />
+      </fieldset>
 
-    <Button class="w-full" type="submit">{details.button}</Button>
-  </form>
+      <Button type="submit">{details.button}</Button>
+    </form>
+  </div>
 </div>
