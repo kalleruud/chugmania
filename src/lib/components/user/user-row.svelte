66c4d4ddfb7087e32fd70a36aad6c7f893bea71d
Ole <ole@kallerud.no>
2025-01-04 17:37:15 +0100

    Implement player list

--- src/lib/components/user/user-row.svelte
+++ src/lib/components/user/user-row.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
+  import { ChevronRight } from 'lucide-svelte'
   import type { PublicUser } from '../types.server'
 
   type Props = {
     class?: string
     user: PublicUser
+    index?: number
   }
 
-  let { class: className, user }: Props = $props()
+  let { class: className, user, index }: Props = $props()
 </script>
 
 <div class={className}>
-  <div class="grid grid-cols-3 gap-4">
-    <p>{user.name}</p>
-    <p>{user.shortName}</p>
+  <div class="flex h-10 items-center justify-between">
+    <div class="flex h-full items-center gap-2">
+      {#if index !== undefined}
+        <p class="font-f1 italic text-muted-foreground">{index}</p>
+      {/if}
+      <h3>{user.name}</h3>
+    </div>
+    <div class="flex gap-2">
+      <p class="font-f1 italic text-muted-foreground">{user.shortName}</p>
+      <ChevronRight class="size-6" />
+    </div>
   </div>
 </div>
--- src/routes/users/+page.svelte
+++ src/routes/users/+page.svelte
@@ -4,22 +4,30 @@
   import Login from '@/components/login/login.svelte'
   import HeaderBar from '@/components/ui/header-bar/header-bar.svelte'
   import UserRow from '@/components/user/user-row.svelte'
+  import { User } from 'lucide-svelte'
   import type { PageData } from './$types'
 
   let { data }: { data: PageData } = $props()
   const { users, loggedInUser } = data
 </script>
 
-<HeaderBar class="px-4 py-2">
+<HeaderBar class="flex items-center justify-between px-4 py-2">
   <h1>Spillere</h1>
+  <a href={'users/' + loggedInUser.id}>
+    <User class="size-6 text-white" />
+  </a>
 </HeaderBar>
 
 <main class="mt-16 flex flex-col p-4">
-  <div class="">
-    {#each users as user}
-      <UserRow {user} />
+  <ul class="divide-y divide-solid">
+    {#each users as user, i}
+      <li>
+        <a href={'users/' + user.id}>
+          <UserRow {user} index={i + 1} />
+        </a>
+      </li>
     {/each}
-  </div>
+  </ul>
 </main>
 
 <form class="fixed mb-16 w-full p-4 bottom-safe" use:enhance method="POST" action="?/logout">
