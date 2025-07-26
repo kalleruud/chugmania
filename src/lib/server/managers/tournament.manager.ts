98d2da19c8d72c9231bbc384b6314f5640422d21
Ole Kallerud <ole@kallerud.no>
2025-07-26 09:27:48 +0200

    Update tournament.manager.ts

--- src/lib/server/managers/tournament.manager.ts
+++ src/lib/server/managers/tournament.manager.ts
@@ -1,17 +1,16 @@
-import type { Session } from '@/components/types.server'
-import GroupManager, { type Group } from './group.manager'
-import MatchManager, { type Match, type NewMatch } from './match.manager'
-import type { Track } from './track.manager'
+import Manager from 'tournament-organizer'
+import type { Tournament } from 'tournament-organizer/components'
+import type { SettableTournamentValues } from 'tournament-organizer/interfaces'
+import type { Session } from './session.manager'
 import type { PublicUser } from './user.manager'
-
-export type BracketRound = Partial<Match> & {
-  user1LastMatch: Partial<Match> | undefined
-  user2LastMatch: Partial<Match> | undefined
-}
+import type { Match } from './match.manager'
 
 export default class TournamentManager {
-  static readonly finalNames = [
-    'Sekstendelsfinale',
+  private static readonly manager = new Manager()
+  private static readonly stageNames = [
+    'R64',
+    'R32',
+    'R16',
     'Åttenedelsfinale',
     'Kvartfinale',
     'Bronsefinale',
@@ -19,4 +18,44 @@ export default class TournamentManager {
     'Finale',
   ]
 
+  private readonly tournament: Tournament
+
+  constructor(
+    session: Session,
+    players: PublicUser[],
+    settings: SettableTournamentValues = {
+      stageOne: {
+        format: 'swiss',
+        consolation: true,
+      },
+      stageTwo: {
+        format: 'double-elimination',
+        consolation: true,
+      },
+    }
+  ) {
+    if (session.type !== 'tournament') throw new Error('Session is not a tournament')
+    if (!session.description) throw new Error('Session is missing description')
+    this.tournament = TournamentManager.manager.createTournament(
+      session.description,
+      settings,
+      session.id
+    )
+    players.forEach(player => this.tournament.createPlayer(player.shortName, player.id))
+  }
+
+  get id() {
+    return this.tournament.id
+  }
+
+  start() {
+    this.tournament.start()
+  }
+
+  get matches() {
+    return this.tournament.matches.map(match => ({
+      ...match,
+      stage: TournamentManager.stageNames[match.],
+    }))
+  }
 }
