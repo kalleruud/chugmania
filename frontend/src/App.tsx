c83227400a43c82f10624bcfcc2202054aadd2a1
Ole Kallerud <ole@kallerud.no>
2025-08-19 16:50:00 +0200

    Delete existing crap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from './vite.svg'
 import { io } from 'socket.io-client'
 
 const socket = io('localhost:6996', {
@@ -13,14 +11,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>
