4bc9fe9f8b427f437b85d9a6e18659f132fdfa0b
Ole Kallerud <ole@kallerud.no>
2025-08-26 13:06:21 +0200

    Fix db issues

--- backend/database/database.ts
+++ backend/database/database.ts
@@ -1,12 +1,9 @@
 import * as schema from '@database/schema.ts'
-import { createClient } from '@libsql/client'
 import { drizzle } from 'drizzle-orm/libsql/node'
 import 'jsr:@std/dotenv/load'
 
 const db_file = Deno.env.get('DB_FILE_NAME')
 if (!db_file) throw Error("Missing environment varible 'DB_FILE_NAME'")
 
-const client = createClient({ url: db_file })
-const db = drizzle(client, { schema })
-
+const db = drizzle({ connection: db_file, schema })
 export default db
--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'drizzle-kit'
+import 'jsr:@std/dotenv/load'
+
+const db_file = Deno.env.get('DB_FILE_NAME')
+if (!db_file) throw Error("Missing environment varible 'DB_FILE_NAME'")
 
 export default defineConfig({
   out: './drizzle',
   dialect: 'sqlite',
-  schema: './schema',
+  schema: './database/schema.ts',
   casing: 'snake_case',
   dbCredentials: {
-    url: 'file:local.db',
+    url: db_file,
   },
 })
