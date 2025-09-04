fc3509d9554e76d322af572f20e54e2bb6d05855
Ole Kallerud <ole@kallerud.no>
2025-09-05 00:46:52 +0200

    Refactor TrackSummary topTimes type
    Replaces the TrackSummary topTimes array from TimeEntry[] to TopTime[], introducing a new TopTime type with user details and omitting certain fields. This improves type safety and clarity for top time entries.

--- common/models/track.ts
+++ common/models/track.ts
@@ -4,7 +4,14 @@ import type { TimeEntry } from "./TimeEntry"
 export type Track = typeof tracks.$inferSelect
 export type CreateTrack = typeof tracks.$inferInsert
 
+export type TopTime = Omit<TimeEntry, 'track' | 'updatedAt' | 'createdAt' | 'deletedAt'> &{
+  user: {
+    id: string
+    name: string
+  }
+}
+
 export type TrackSummary = Omit<Track, 'updatedAt' | 'createdAt' | 'deletedAt'> & {
   lapCount: number
-  topTimes: TimeEntry[]
+  topTimes: TopTime[]
 }
