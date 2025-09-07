a14722ab828f0ba34e083266a1a5344b9cc19d1f
Ole Kallerud <ole@kallerud.no>
2025-09-08 01:35:28 +0200

    Refine TimeEntryGap and export TimeEntryDetails type
    Updated the TimeEntryGap type to support union types for more flexible gap representation. Also changed TimeEntryDetails to be exported, making it available for use in other modules.

--- common/models/timeEntry.ts
+++ common/models/timeEntry.ts
@@ -4,13 +4,21 @@ import type { UserInfo } from './user'
 
 export type TimeEntry = typeof timeEntries.$inferSelect
 export type CreateTimeEntry = typeof timeEntries.$inferInsert
-export type TimeEntryGap = {
-  next: number | null
-  previous: number | null
-  leader: number | null
-}
+export type TimeEntryGap =
+  | {
+      position?: number
+      next?: number
+      previous?: number
+      leader?: number
+    }
+  | {
+      position?: 1
+      next?: never
+      previous?: number
+      leader?: never
+    }
 
-type TimeEntryDetails = Omit<TimeEntry, 'user' | 'track'> & {
+export type TimeEntryDetails = Omit<TimeEntry, 'user' | 'track'> & {
   user: UserInfo
   track: Track
   gap: TimeEntryGap
