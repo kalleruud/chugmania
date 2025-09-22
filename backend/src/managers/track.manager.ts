b38a666fa47048c1d5ddc9f53328aad8a120b0dd
Ole Kallerud <ole@kallerud.no>
2025-09-22 22:11:53 +0200

    Remove track seeding

--- backend/src/managers/track.manager.ts
+++ backend/src/managers/track.manager.ts
@@ -3,33 +3,11 @@ import type {
   BackendResponse,
   GetTracksResponse,
 } from '../../../common/models/responses'
-import { TRACK_LEVELS, TRACK_TYPES } from '../../../common/models/track'
 import { tryCatchAsync } from '../../../common/utils/try-catch'
 import db from '../../database/database'
 import { timeEntries, tracks } from '../../database/schema'
 
 export default class TrackManager {
-  static async seed(): Promise<void> {
-    const { data, error } = await tryCatchAsync(db.query.tracks.findFirst())
-    if (error) throw error
-    if (data) return
-
-    const trackCount = 200
-
-    const items: (typeof tracks.$inferInsert)[] = []
-    for (let i = 0; i < trackCount; i++) {
-      items.push({
-        number: i + 1,
-        level: TRACK_LEVELS.filter(t => t != 'custom')[
-          Math.floor(i / 40) % (TRACK_LEVELS.length - 1)
-        ]!,
-        type: TRACK_TYPES[Math.floor(i / 10) % TRACK_TYPES.length]!,
-      })
-    }
-    await db.insert(tracks).values(items)
-    console.log(`Inserted ${trackCount} tracks`)
-  }
-
   static async getTrackIdsWithLapTimes(
     offset = 0,
     limit = 100
