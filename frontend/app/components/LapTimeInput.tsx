b7301691ce160093a45a461b7228f6ff8b84854b
Ole Kallerud <ole@kallerud.no>
2025-10-08 20:07:59 +0200

    Fix parseInt/Float warning

--- frontend/app/components/LapTimeInput.tsx
+++ frontend/app/components/LapTimeInput.tsx
@@ -155,14 +155,14 @@ export default function LapTimeInput({
   }, [socket])
 
   function getMs() {
-    const tenMinutes = digits[0] !== '' ? parseInt(digits[0]) * 10 : 0
-    const minutes = digits[1] !== '' ? parseInt(digits[1]) : 0
+    const tenMinutes = digits[0] !== '' ? Number.parseInt(digits[0]) * 10 : 0
+    const minutes = digits[1] !== '' ? Number.parseInt(digits[1]) : 0
 
-    const tenSeconds = digits[2] !== '' ? parseInt(digits[2]) * 10 : 0
-    const seconds = digits[3] !== '' ? parseInt(digits[3]) : 0
+    const tenSeconds = digits[2] !== '' ? Number.parseInt(digits[2]) * 10 : 0
+    const seconds = digits[3] !== '' ? Number.parseInt(digits[3]) : 0
 
-    const tenHundredths = digits[5] !== '' ? parseInt(digits[5]) * 10 : 0
-    const hundredths = digits[4] !== '' ? parseInt(digits[4]) : 0
+    const tenHundredths = digits[5] !== '' ? Number.parseInt(digits[5]) * 10 : 0
+    const hundredths = digits[4] !== '' ? Number.parseInt(digits[4]) : 0
 
     return formattedTimeToMs(
       tenMinutes + minutes,
