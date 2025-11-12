f55bb624dcf2b0b53677be11a2a1a023358eea23
kalleruud <ole@kallerud.no>
2025-11-12 01:54:28 +0100

    Add toggling interval

--- frontend/app/pages/TrackPage.tsx
+++ frontend/app/pages/TrackPage.tsx
@@ -2,6 +2,7 @@ import TimeEntryItem, {
   type GapType,
 } from '@/components/timeentries/TimeEntryItem'
 import { TrackItem } from '@/components/track/TrackItem'
+import { Button } from '@/components/ui/button'
 import { Empty } from '@/components/ui/empty'
 import { Spinner } from '@/components/ui/spinner'
 import { useData } from '@/contexts/DataContext'
@@ -16,7 +17,7 @@ function RowItemList({
   track,
   entries,
 }: Readonly<{ track: Track; entries: LeaderboardEntry[] }>) {
-  const [gapType, setGapType] = useState<GapType>('leader')
+  const [gapType, setGapType] = useState<GapType>('interval')
   const { open } = useTimeEntryDrawer()
 
   if (entries.length === 0) {
@@ -27,20 +28,33 @@ function RowItemList({
     )
   }
   return (
-    <div className='bg-background-secondary flex flex-col rounded-sm'>
-      {entries.map(entry => (
-        <TimeEntryItem
-          key={entry.id}
-          lapTime={entry}
-          onClick={() => open({ track: track.id, ...entry })}
-          className='p-4 py-3 first:pt-4 last:pb-4'
-          gapType={gapType}
-          onChangeGapType={() =>
-            setGapType(gapType === 'leader' ? 'interval' : 'leader')
-          }
-        />
-      ))}
-    </div>
+    <>
+      <div className='flex justify-between'>
+        <Button
+          variant='outline'
+          size='sm'
+          className='w-24'
+          onClick={() =>
+            setGapType(gapType === 'interval' ? 'leader' : 'interval')
+          }>
+          {gapType}
+        </Button>
+      </div>
+      <div className='bg-background-secondary flex flex-col rounded-sm'>
+        {entries.map(entry => (
+          <TimeEntryItem
+            key={entry.id}
+            lapTime={entry}
+            onClick={() => open({ track: track.id, ...entry })}
+            className='p-4 py-3 first:pt-4 last:pb-4'
+            gapType={gapType}
+            onChangeGapType={() =>
+              setGapType(gapType === 'leader' ? 'interval' : 'leader')
+            }
+          />
+        ))}
+      </div>
+    </>
   )
 }
 
@@ -63,8 +77,8 @@ export default function TrackPage() {
   const leaderboard = id in leaderboards ? leaderboards[id].entries : []
 
   return (
-    <div className='flex flex-col p-2'>
-      <TrackItem track={track} variant='card' />
+    <div className='flex flex-col gap-2 p-2'>
+      <TrackItem track={track} variant='card' className='pb-0' />
       <RowItemList track={track} entries={leaderboard} />
     </div>
   )
