2f4607fde46e220136579833dbf27e2ea42beb3d
kalleruud <ole@kallerud.no>
2025-11-16 18:36:19 +0100

    Revert "Create bundle fonts plugin"
    This reverts commit c682810bc70ba4ffd46b1d4dba477b2c5d30757b.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import fs from 'node:fs'
 import path from 'node:path'
 import { defineConfig } from 'vite'
-import serveFontsPlugin from './vite-plugin-serve-fonts'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,29 +12,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    serveFontsPlugin(),
-    {
-      name: 'copy-titillium-fonts',
-      apply: 'build',
-      generateBundle() {
-        const srcDir = path.resolve(
-          __dirname,
-          'node_modules/@fontsource/titillium-web/files'
-        )
-        const files = fs
-          .readdirSync(srcDir)
-          .filter(f => f.endsWith('.woff') || f.endsWith('.woff2'))
-
-        files.forEach(file => {
-          const content = fs.readFileSync(path.join(srcDir, file))
-          this.emitFile({
-            type: 'asset',
-            fileName: `fonts/titillium-web/${file}`,
-            source: content,
-          })
-        })
-      },
-    },
   ],
   resolve: {
     alias: {
