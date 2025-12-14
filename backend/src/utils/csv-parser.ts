fcd976dd6559c792a4debba5b8048b431f1801be
kalleruud <ole@kallerud.no>
2025-12-14 11:21:58 +0100

    Parallelize csv parsing

--- backend/src/utils/csv-parser.ts
+++ backend/src/utils/csv-parser.ts
@@ -8,21 +8,22 @@ export default class CsvParser {
     const [headerLine, ...lines] = csv.trim().split('\n')
     const headers = headerLine.split(',')
 
-    const objects: object[] = []
-    for (const line of lines) {
-      const values = line.split(',')
-      const entries = new Map()
+    const objects = await Promise.all(
+      lines.map(async line => {
+        const values = line.split(',')
+        const entries = new Map()
 
-      for (let i = 0; i < headers.length; i++) {
-        const k = headers.at(i)?.trim()
-        if (!k) continue
-        const { key, value } = await CsvParser.normalize(k, values.at(i))
-        if (value === null) continue
-        entries.set(key, value)
-      }
+        for (let i = 0; i < headers.length; i++) {
+          const k = headers.at(i)?.trim()
+          if (!k) continue
+          const { key, value } = await CsvParser.normalize(k, values.at(i))
+          if (value === null) continue
+          entries.set(key, value)
+        }
 
-      objects.push(Object.fromEntries(entries))
-    }
+        return Object.fromEntries(entries)
+      })
+    )
 
     return objects as T[]
   }
