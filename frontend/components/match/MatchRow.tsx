fcc64b078db684acb31344962eb9be2cae886dea
kalleruud <ole@kallerud.no>
2025-12-23 16:30:41 +0100

    Update MatchRow layout and visibility logic for improved responsiveness
    - Added margin-top to the grid container in MatchRow for better spacing.
    - Enhanced visibility logic for track information, conditionally hiding elements based on session and match stage status.

--- frontend/components/match/MatchRow.tsx
+++ frontend/components/match/MatchRow.tsx
@@ -86,7 +86,7 @@ export default function MatchRow({
         highlight && 'bg-foreground/13'
       )}
       {...rest}>
-      <div className='grid w-full grid-cols-1 items-center gap-1 sm:grid-cols-2'>
+      <div className='mt-1 grid w-full grid-cols-1 items-center gap-1 sm:grid-cols-2'>
         <div
           className={twMerge(
             'flex w-full items-center justify-center gap-2',
@@ -121,7 +121,11 @@ export default function MatchRow({
           />
         </div>
 
-        <div className='flex items-center justify-center gap-2 sm:justify-start'>
+        <div
+          className={twMerge(
+            'flex items-center justify-center gap-2 sm:justify-start',
+            (!track || hideTrack) && !session && !match.stage && 'hidden'
+          )}>
           {track && !hideTrack && (
             <div className='flex items-center gap-2'>
               <span
