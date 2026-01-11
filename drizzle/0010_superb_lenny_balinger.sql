675d7f58374a57e768c774b068d3d5c224ce26e7
kalleruud <ole@kallerud.no>
2026-01-11 22:06:56 +0100

    Remove db migs

--- drizzle/0010_superb_lenny_balinger.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `tournament_matches` ADD `position` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `tournament_matches` ADD `group` text REFERENCES groups(id);--> statement-breakpoint
-ALTER TABLE `tournament_matches` DROP COLUMN `name`;
--- drizzle/0011_daffy_riptide.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `group_players` ADD `totalMatches` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `group_players` ADD `wins` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `group_players` ADD `losses` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `groups` ADD `number` integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE `groups` DROP COLUMN `name`;
