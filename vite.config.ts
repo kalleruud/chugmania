e32c8d369130f8fb50c78f8be304918a8f829906
kalleruud <ole@kallerud.no>
2026-01-19 02:38:26 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'lcov'],
       reportsDirectory: './coverage',
-      include: ['backend/**/*.ts', 'common/**/*.ts', 'frontend/**/*.ts'],
+      include: ['backend/**/*.ts'],
       exclude: [
         'node_modules/**',
         'dist/**',
