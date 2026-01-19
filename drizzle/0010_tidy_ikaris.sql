c12b9a81666036d24aff85d7c1f980dd28e92c2e
kalleruud <ole@kallerud.no>
2026-01-19 19:51:26 +0100

    Update 0010_tidy_ikaris.sql

--- drizzle/0010_tidy_ikaris.sql
+++ drizzle/0010_tidy_ikaris.sql
@@ -1,18 +1,5 @@
-CREATE TABLE `match_dependencies` (
-	`id` text PRIMARY KEY NOT NULL,
-	`updated_at` integer,
-	`created_at` integer NOT NULL,
-	`deleted_at` integer,
-	`from_match` text,
-	`from_group` text,
-	`to_match` text NOT NULL,
-	`from_position` integer NOT NULL,
-	`to_slot` text NOT NULL,
-	FOREIGN KEY (`from_match`) REFERENCES `matches`(`id`) ON UPDATE no action ON DELETE cascade,
-	FOREIGN KEY (`from_group`) REFERENCES `groups`(`id`) ON UPDATE no action ON DELETE cascade,
-	FOREIGN KEY (`to_match`) REFERENCES `matches`(`id`) ON UPDATE no action ON DELETE cascade
-);
---> statement-breakpoint
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+DROP TABLE `tournament_matches`;--> statement-breakpoint
 CREATE TABLE `stages` (
 	`id` text PRIMARY KEY NOT NULL,
 	`updated_at` integer,
@@ -25,8 +12,6 @@ CREATE TABLE `stages` (
 	FOREIGN KEY (`tournament`) REFERENCES `tournaments`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-DROP TABLE `tournament_matches`;--> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
 CREATE TABLE `__new_matches` (
 	`id` text PRIMARY KEY NOT NULL,
 	`updated_at` integer,
@@ -50,10 +35,9 @@ CREATE TABLE `__new_matches` (
 	FOREIGN KEY (`stage`) REFERENCES `stages`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-INSERT INTO `__new_matches`("id", "updated_at", "created_at", "deleted_at", "user_a", "user_b", "winner", "track", "session", "status", "duration_ms", "completed_at", "comment", "index", "stage") SELECT "id", "updated_at", "created_at", "deleted_at", "user_a", "user_b", "winner", "track", "session", "status", "duration_ms", "completed_at", "comment", "index", "stage" FROM `matches`;--> statement-breakpoint
+INSERT INTO `__new_matches`("id", "updated_at", "created_at", "deleted_at", "user_a", "user_b", "winner", "track", "session", "status", "duration_ms", "completed_at", "comment", "index", "stage") SELECT "id", "updated_at", "created_at", "deleted_at", "user1", "user2", "winner", "track", "session", "status", "duration_ms", NULL, "comment", NULL, NULL FROM `matches`;--> statement-breakpoint
 DROP TABLE `matches`;--> statement-breakpoint
 ALTER TABLE `__new_matches` RENAME TO `matches`;--> statement-breakpoint
-PRAGMA foreign_keys=ON;--> statement-breakpoint
 CREATE TABLE `__new_tournaments` (
 	`id` text PRIMARY KEY NOT NULL,
 	`updated_at` integer,
@@ -71,5 +55,21 @@ CREATE TABLE `__new_tournaments` (
 INSERT INTO `__new_tournaments`("id", "updated_at", "created_at", "deleted_at", "session", "name", "description", "groups_count", "advancement_count", "elimination_type") SELECT "id", "updated_at", "created_at", "deleted_at", "session", "name", "description", "groups_count", "advancement_count", "elimination_type" FROM `tournaments`;--> statement-breakpoint
 DROP TABLE `tournaments`;--> statement-breakpoint
 ALTER TABLE `__new_tournaments` RENAME TO `tournaments`;--> statement-breakpoint
+CREATE TABLE `match_dependencies` (
+	`id` text PRIMARY KEY NOT NULL,
+	`updated_at` integer,
+	`created_at` integer NOT NULL,
+	`deleted_at` integer,
+	`from_match` text,
+	`from_group` text,
+	`to_match` text NOT NULL,
+	`from_position` integer NOT NULL,
+	`to_slot` text NOT NULL,
+	FOREIGN KEY (`from_match`) REFERENCES `matches`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`from_group`) REFERENCES `groups`(`id`) ON UPDATE no action ON DELETE cascade,
+	FOREIGN KEY (`to_match`) REFERENCES `matches`(`id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
 ALTER TABLE `groups` ADD `index` integer NOT NULL;--> statement-breakpoint
 ALTER TABLE `groups` DROP COLUMN `name`;
