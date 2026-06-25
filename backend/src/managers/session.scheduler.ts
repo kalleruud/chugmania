dc32ac390ccac5f45436a5f9b42e9944f3860147
kalleruud <ole@kallerud.no>
2026-06-25 21:01:16 +0200

    Update session.scheduler.ts

--- backend/src/managers/session.scheduler.ts
+++ backend/src/managers/session.scheduler.ts
@@ -9,7 +9,6 @@ export default class SessionScheduler {
   private static readonly MAX_TIMEOUT_MS = 2 ** 31 - 1
 
   private static scheduledTimeout: NodeJS.Timeout | null = null
-  private static checkTimeout: NodeJS.Timeout | null = null
 
   static async start(): Promise<void> {
     const nextSession = await SessionScheduler.findNextSession()
@@ -24,16 +23,14 @@ export default class SessionScheduler {
 
     const delayMs = Math.max(nextSession.date.getTime() - Date.now())
 
-    if (delayMs < SessionScheduler.MAX_TIMEOUT_MS) {
-      SessionScheduler.reschedule(delayMs)
-      console.debug(
-        new Date().toISOString(),
-        `Scheduled session "${nextSession.name}" to start in ${delayMs}ms`,
-        nextSession.id
-      )
-    }
-
-    return SessionScheduler.rescheduleCheck()
+    SessionScheduler.reschedule(
+      Math.min(delayMs, SessionScheduler.MAX_TIMEOUT_MS)
+    )
+    console.debug(
+      new Date().toISOString(),
+      `Scheduled session "${nextSession.name}" to start in ${delayMs}ms`,
+      nextSession.id
+    )
   }
 
   private static async findNextSession(): Promise<SessionWithSignups | null> {
@@ -48,15 +45,6 @@ export default class SessionScheduler {
     return SessionManager.getSession(nextSessionRow.id)
   }
 
-  private static async onSessionStart(): Promise<void> {
-    console.debug(
-      new Date().toISOString(),
-      'Session started, broadcasting all_sessions'
-    )
-
-    broadcast('all_sessions', await SessionManager.getAllSessions())
-  }
-
   private static reschedule(delay: number): void {
     if (SessionScheduler.scheduledTimeout) {
       clearTimeout(SessionScheduler.scheduledTimeout)
@@ -68,16 +56,13 @@ export default class SessionScheduler {
     )
   }
 
-  private static rescheduleCheck(): void {
-    if (SessionScheduler.checkTimeout) {
-      clearTimeout(SessionScheduler.checkTimeout)
-    }
-
-    SessionScheduler.checkTimeout = setTimeout(
-      SessionScheduler.start,
-      SessionScheduler.MAX_TIMEOUT_MS
+  private static async onSessionStart(): Promise<void> {
+    console.debug(
+      new Date().toISOString(),
+      'Session started, broadcasting all_sessions'
     )
 
-    console.debug(new Date().toISOString(), 'Rescheduled check')
+    broadcast('all_sessions', await SessionManager.getAllSessions())
+    SessionScheduler.start()
   }
 }
